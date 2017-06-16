09b0ec01cf2271b518ec1e0d2ab698f6f5bbbaac
Kristin Riebe <kriebe@aip.de>
2017-06-16 10:25:12 +0200

    Append current path to find settings

--- provenance/wsgi.py
+++ provenance/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# append path, so the settigns file can be found
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "provenance.settings")
 
